9205e252b5218bf3cafd7ed248a6393d6bfbd8ce
AlexanderArj <aromerojuliao@byupathway.edu>
2026-01-17 14:45:34 -0300

    assignment2-sql statements

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -234,3 +234,17 @@ VALUES (
         'White',
         5
     );
+
+-- 4
+
+UPDATE public.inventory
+SET
+  inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+-- 6
+
+UPDATE public.inventory
+SET
+  inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+  inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
