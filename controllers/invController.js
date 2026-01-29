4c8dd1081fc04977ba768fc57071f6612bc81aaa
AlexanderArj <aromerojuliao@byupathway.edu>
2026-01-29 19:00:47 -0300

    week 3 complete

--- controllers/invController.js
+++ controllers/invController.js
@@ -7,16 +7,56 @@ const invCont = {}
  *  Build inventory by classification view
  * ************************** */
 invCont.buildByClassificationId = async function (req, res, next) {
-  const classification_id = req.params.classificationId
-  const data = await invModel.getInventoryByClassificationId(classification_id)
-  const grid = await utilities.buildClassificationGrid(data)
-  let nav = await utilities.getNav()
-  const className = data[0].classification_name
-  res.render("./inventory/classification", {
-    title: className + " vehicles",
-    nav,
-    grid,
-  })
+
+  try {
+    const classification_id = req.params.classificationId
+    const data = await invModel.getInventoryByClassificationId(classification_id)
+    const grid = await utilities.buildClassificationGrid(data)
+    let nav = await utilities.getNav()
+    const className = data[0].classification_name
+    
+    res.render("./inventory/classification", {
+      title: className + " vehicles",
+      nav,
+      grid,
+    })
+
+  } catch(error) {
+    next(error)
+  }
+}
+
+// Build inventory by invID
+
+invCont.buildDetail = async function (req, res, next) {
+  try {
+    const invId = req.params.invId
+
+    const data = await invModel.getInventoryByInvId(invId)
+    const detailContainer = await utilities.buildDetailContainer(data)
+    let nav = await utilities.getNav()
+    const className = data[0].classification_name
+
+    res.render("./inventory/detail", {
+      title: `${data[0].inv_make} ${data[0].inv_model}`,
+      nav,
+      detailContainer,
+    })
+    
+  } catch (error) {
+    next(error)
+  }
+}
+
+
+// Intentional error
+invCont.getError = async function (req, res, next) {
+
+  try {
+    throw new Error("Intentional error for testing")
+  } catch (error) {
+    next(error)
+  }
 }
 
 module.exports = invCont
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,8 +2,17 @@
 const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
+const utilities = require("../utilities/")
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
+
+// Route to build inventory by invID, vehicle details
+
+router.get("/detail/:invId", utilities.handleErrors(invController.buildDetail));
+
+// Route to trigger an intentional error
+router.get("/trigger-error", utilities.handleErrors(invController.triggerError));
 
 module.exports = router;
+
