cb2925e6b8d5757e1139de69b20b1026991b7999
AlexanderArj <aromerojuliao@byupathway.edu>
2026-02-11 16:16:03 -0300

    Select Inventory Items Learning act. 1 - Working

--- controllers/invController.js
+++ controllers/invController.js
@@ -57,15 +57,20 @@ invCont.getError = async function (req, res, next) {
   }
 }
 
+// responsible for delivering the inventory management view.
 
 invCont.buildManagement = async function (req, res, next) {
   let nav = await utilities.getNav()
-  const messages = req.flash('notice') 
+  const messages = req.flash('notice')
+
+  const classificationSelect = await utilities.buildClassificationList()
   
   res.render("./inventory/management", {
     title: "Vehicle Management",
     nav,
-    messages, 
+    messages,
+    classificationSelect,
+    // errors: null?
   })
 }
 
@@ -138,6 +143,19 @@ invCont.addInventory = async function (req, res) {
       classificationSelect,
       errors: null,
     })
+  }
+}
+
+/* ***************************
+ *  Return Inventory by Classification As JSON
+ * ************************** */
+invCont.getInventoryJSON = async (req, res, next) => {
+  const classification_id = parseInt(req.params.classification_id)
+  const invData = await invModel.getInventoryByClassificationId(classification_id)
+  if (invData[0].inv_id) {
+    return res.json(invData)
+  } else {
+    next(new Error("No data returned"))
   }
 }
 
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -41,5 +41,9 @@ router.post(
   utilities.handleErrors(invController.addInventory)
 )
 
+// 
+
+router.get("/getInventory/:classification_id", utilities.handleErrors(invController.getInventoryJSON))
+
 module.exports = router;
 
